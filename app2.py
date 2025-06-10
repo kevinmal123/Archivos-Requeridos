113f31eb892d6f531abb0b1fcd2598e1fb363cde
kevinmal123 <kevin.malamboa@gmail.com>
2025-06-09 20:28:37 -0500

    kevin_viz_csmbio

--- app2.py
+++ app2.py
@@ -41,7 +41,7 @@ def plot_series(data, initial_date, proy):
             x=data_plot.index,
             y=data_plot['AT_load_actual_entsoe_transparency'],
             mode='lines',
-            line=dict(color="#188463"),
+            line=dict(color="#188663"),
         ),
         go.Scatter(
             name='Proyección',
