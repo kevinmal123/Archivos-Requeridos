bb4084f12b7a6682e3bae5ae18016c9123c354bb
kevinmal123 <kevin.malamboa@gmail.com>
2025-06-09 20:19:15 -0500

    app con direccion

--- app.py
+++ app.py
@@ -21,7 +21,11 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    data = pd.read_csv("datos_energia.csv")
+    data["time"] = pd.to_datetime(data["time"], format="%Y-%m-%d %H:%M:%S")
+    data.set_index("time", inplace=True)
+    data
+    return data
     
 
 # Cargar datos
